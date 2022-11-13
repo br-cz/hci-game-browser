925240e1acca69412c3e5e05273bd6ff6192bffa
Bricz <bricz.cruz@gmail.com>
2022-11-12 22:50:32 -0600

    added db play test files and elements

--- js/accessDB.js
+++ js/accessDB.js
@@ -6,24 +6,26 @@ function testDB() {
     //This retrieves the actionDB
     var actionGames = JSON.parse(localStorage.getItem('actionDB'));
 
-    //This finds the first element with a div tag in HTML document and adds to its inner html with the ith game title as well as a line break
+    //This finds the first element with a #accessDB tag in HTML document and adds to its inner html with the ith game title as well as a line break
     for (let i = 0; i < 5; i++) {
-      document.querySelector('div').innerHTML += actionGames[i].title + '<br>';
+      document.querySelector('#accessDB').innerHTML +=
+        actionGames[i].title + '<br>';
     }
 
     //This justs adds another line break so we can see the changes in the next loop
-    document.querySelector('div').innerHTML += '<br>';
+    document.querySelector('#accessDB').innerHTML += '<br>';
 
     for (let i = 0; i < 5; i++) {
       //This adds the ith number to the ith game in the action database
       actionGames[i].title += ' ' + i;
 
-      //This finds the first element with a div tag in HTML document and adds to its inner html with the ith game title as well as a line break
-      document.querySelector('div').innerHTML += actionGames[i].title + '<br>';
+      //This finds the first element with a #accessDB tag in HTML document and adds to its inner html with the ith game title as well as a line break
+      document.querySelector('#accessDB').innerHTML +=
+        actionGames[i].title + '<br>';
     }
 
     //This justs adds another line break so we can see the changes in the next loop
-    document.querySelector('div').innerHTML += '<br>';
+    document.querySelector('#accessDB').innerHTML += '<br>';
 
     //This updates the actionDB by once again stringifying the now modified actionDB
     localStorage.setItem('actionDB', JSON.stringify(actionGames));
@@ -33,7 +35,8 @@ function testDB() {
 
     //This should show that the game titles have been modified
     for (let i = 0; i < 5; i++) {
-      document.querySelector('div').innerHTML += actionGames2[i].title + '<br>';
+      document.querySelector('#accessDB').innerHTML +=
+        actionGames2[i].title + '<br>';
     }
   } else {
     console.log('empty');
