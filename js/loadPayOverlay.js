f04b6b500d43290b01c8541295d8264513cb2533
Bricz <bricz.cruz@gmail.com>
2022-11-27 13:19:59 -0600

    added unimplemented game settings feature on library

--- js/loadPayOverlay.js
+++ js/loadPayOverlay.js
@@ -37,11 +37,13 @@ function openStoreGame(title) {
     '" class="d-block w-100"/>' +
     '</div>';
 
-  passTitle = currentGame.title.replaceAll("'","");
+  passTitle = currentGame.title.replaceAll("'", '');
 
   const buttonRow =
     '<div id="buttonRowOuter">' +
-    '<div class="buttonRowInner"><button class="playGameButton" onclick="purchaseSinglePrompt(\'' + passTitle + '\')">PURCHASE</button></div>' +
+    '<div class="buttonRowInner"><button class="playGameButton" onclick="purchaseSinglePrompt(\'' +
+    passTitle +
+    '\')">PURCHASE</button></div>' +
     '</div>';
 
   const gameDescription =
@@ -50,36 +52,53 @@ function openStoreGame(title) {
     currentGame.description +
     '</textarea>';
 
-  salePrice = (1 - (currentGame.sale/100)) * currentGame.price; 
-   var priceRow;
-   
-   if(!currentGame.cart)
-   {
+  salePrice = (1 - currentGame.sale / 100) * currentGame.price;
+  var priceRow;
+
+  if (!currentGame.cart) {
     console.log(passTitle);
-      priceRow = '<div id="buttonRowOuterPrice">' +
-                '<div class="buttonRowPayOverlay"><button class="addCartWishlist" onclick="addToCart(\'' + passTitle + '\');openStoreGame(\'' + passTitle + '\')")>Add to Cart</button></div>' +
-                '<div class="gamePrice">Price: $' + salePrice.toFixed(2) + '</div>';
-   } 
-   else
-   {
-    priceRow = '<div id="buttonRowOuterPrice">' +
-    '<div class="buttonRowPayOverlay"><button class="addCartWishlist" onclick="removeFromCart(\'' + passTitle + '\');openStoreGame(\'' + passTitle + '\')")>Remove from Cart</button></div>' +
-    '<div class="gamePrice">Price: $' + salePrice.toFixed(2) + '</div>';
-   }
+    priceRow =
+      '<div id="buttonRowOuterPrice">' +
+      '<div class="buttonRowPayOverlay"><button class="addCartWishlist" onclick="addToCart(\'' +
+      passTitle +
+      "');openStoreGame('" +
+      passTitle +
+      '\')")>Add to Cart</button></div>' +
+      '<div class="gamePrice">Price: $' +
+      salePrice.toFixed(2) +
+      '</div>';
+  } else {
+    priceRow =
+      '<div id="buttonRowOuterPrice">' +
+      '<div class="buttonRowPayOverlay"><button class="addCartWishlist" onclick="removeFromCart(\'' +
+      passTitle +
+      "');openStoreGame('" +
+      passTitle +
+      '\')")>Remove from Cart</button></div>' +
+      '<div class="gamePrice">Price: $' +
+      salePrice.toFixed(2) +
+      '</div>';
+  }
 
-   var wishlistButton;
+  var wishlistButton;
 
-   if(!currentGame.wishlist)
-   {
-    priceRow += '<div class="buttonRowPayOverlay"><button class="addCartWishlist" onclick="addToWish(\'' + passTitle + '\');openStoreGame(\'' + passTitle + '\')">Add to Wishlist</button></div>' +
-    '</div>';
-   }
-   else
-   {
-    priceRow += '<div class="buttonRowPayOverlay"><button class="addCartWishlist" onclick="removeFromWish(\'' + passTitle + '\');openStoreGame(\'' + passTitle + '\')">Remove from Wishlist</button></div>' +
-    '</div>';
-   }
-   
+  if (!currentGame.wishlist) {
+    priceRow +=
+      '<div class="buttonRowPayOverlay"><button class="addCartWishlist" onclick="addToWish(\'' +
+      passTitle +
+      "');openStoreGame('" +
+      passTitle +
+      '\')">Add to Wishlist</button></div>' +
+      '</div>';
+  } else {
+    priceRow +=
+      '<div class="buttonRowPayOverlay"><button class="addCartWishlist" onclick="removeFromWish(\'' +
+      passTitle +
+      "');openStoreGame('" +
+      passTitle +
+      '\')">Remove from Wishlist</button></div>' +
+      '</div>';
+  }
 
   var inCart = '';
 
@@ -92,15 +111,14 @@ function openStoreGame(title) {
   document.querySelector('.popup').innerHTML =
     '<div id="overlay_exit_button" onclick="closeOverlay()">' +
     '<button class="buttonCircle">x</button> </div> </div>' +
-    
     inCart +
-     gameTitle +
-     gameImage +
-     priceRow +
-     gameDescription +
-     buttonRow;
-    // buttonRow +
-    // gameDescription;
+    gameTitle +
+    gameImage +
+    priceRow +
+    gameDescription +
+    buttonRow;
+  // buttonRow +
+  // gameDescription;
 
   window.onclick = function (event) {
     if (event.target == overlay) {
